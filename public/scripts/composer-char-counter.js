b9b0eda74950f7a5dcfd78607baaf5fc236beac7
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-10-05 03:14:27 +0000

    adds counter and red class style

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    const tweetText = document.getElementById("tweet-text");
+
+    tweetText.addEventListener("input", function () {
+        let char = this.value.length;
+        let counter = 140 - char;
+
+        document.getElementsByClassName("counter")[0].innerHTML = counter;
+
+        let styleChange = document.getElementsByClassName("counter")[0];
+        if (counter <= -1) {
+            styleChange.classList.add('red');
+        }
+
+        if (counter >= 0) {
+            styleChange.classList.remove('red');
+        }
+    });
+
+
+
+});
