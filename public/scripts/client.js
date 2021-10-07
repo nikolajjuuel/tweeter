5e307f74a3190674eb9d1386eb3678b934b39270
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-10-07 23:13:42 +0000

    adds jquery validator and error messages

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function () {
 
     const loadTweets = () => {
@@ -9,6 +7,7 @@ $(document).ready(function () {
             dataType: 'json',
             success: function (data) {
                 console.log(data);
+                renderTweets(data);
             },
             error: function (err) {
                 console.err(err);
@@ -20,78 +19,75 @@ $(document).ready(function () {
     loadTweets();
 
 
-    const data = [
-        {
-            "user": {
-                "name": "Newton",
-                "avatars": "https://i.imgur.com/73hZDYK.png"
-                ,
-                "handle": "@SirIsaac"
-            },
-            "content": {
-                "text": "If I have seen further it is by standing on the shoulders of giants"
-            },
-            "created_at": 1461116232227
-        },
-        {
-            "user": {
-                "name": "Descartes",
-                "avatars": "https://i.imgur.com/nlhLi3I.png",
-                "handle": "@rd"
-            },
-            "content": {
-                "text": "Je pense , donc je suis"
-            },
-            "created_at": 1461113959088
-        }
-    ]
-
-
     const createTweetElement = function (obj) {
         const $location = $('#tweets-container');
         const $header = $(`<header> <div> <p><img src="${obj.user.avatars}" alt=""> ${obj.user.name}</p><p class="handle">${obj.user.handle}</p></div></header>`);
-
         const $div = $('<div>');
-        const $footer = $('<footer>');
 
+        const $formattedDate = timeago.format(obj.created_at);
+        
+        const $footer = $(`<footer><div class="date need_to_be_rendered" datetime="1633639091411">${$formattedDate}</div><div class="logos"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i> </div></footer>`)
+
+
+        //const $footer = $('<footer>');
         const $article = $('<article>');
-        const $logos = $(`<div class="logos"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i> </div>`)
-        const $date = $(`<div class="date need_to_be_rendered" datetime="${obj.created_at}"></div>`);
+       // const $logos = $(`<div class="logos"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i> </div>`)
+       // const $date = $(`<div class="date need_to_be_rendered" datetime="${obj.created_at}">${$formattedDate}</div>`);
+       // console.log('DATE',$date);
+
         const $content = $(`<p>${obj.content.text}</p>`);
         const $tweetInfo = $div.append($content);
-        const $footerInfo = $footer.append($date, $logos);
+        //const $footerInfo = $footer.append($date, $logos);
+        const $articles = $article.append($header, $tweetInfo, $footer);
 
-        const $articles = $article.append($header, $tweetInfo, $footerInfo);
-
-        const $complete = $location.append($articles);
+        const $complete = $location.prepend($articles);
         return $complete;
     };
 
 
     const renderTweets = function (tweets) {
+        $('#tweets-container').empty();
+
         for (const tweet in tweets) {
-            // console.log(tweets[tweet]);
             createTweetElement(tweets[tweet]);
         }
-
     }
 
-    renderTweets(data);
-
-    timeago.render(document.querySelectorAll('.need_to_be_rendered'));
-
     const $form = $('#new-tweet');
+
+
 
     $form.on('submit', function (e) {
         e.preventDefault();
 
-        const serializeData = $(this).serialize();
 
-        $.post('/tweets', serializeData, (r) => {
-            console.log(r);
-            console.log('yes');
-        })
+        $('#new-tweet').validate({
+            rules: {
+                text: {
+                    required: true,
+                    minlength: 1,
+                    maxlength: 140
+                },
+            },
+            messages: {
+                text: {
+                    required: 'Please insert your deep thought above',
+                    minlength: 'Please insert your thought above',
+                    maxlength: 'This hum is too long, please shorten it!'
+                },
+                
+            },
+            submitHandler: function (form) {   
+                console.log('form',form); 
+                console.log()
+
+                const serializedData = $(form).serialize();
+                $.post('/tweets', serializedData).then(loadTweets).then($('#tweet-text').val(''),console.log('test'));
+            }
+        });
+
         
     });
 
 })
+
