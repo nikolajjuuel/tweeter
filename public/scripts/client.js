a1a8f42661ccacab88cf784173eceaa2336b2e11
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-10-07 05:13:50 +0000

    adds form and post request to server /tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,56 +1,97 @@
+
+
 $(document).ready(function () {
-    // $.ajax({
-    //     url: '/initial-tweets.json',
-    //     method: 'GET',
-    //     dataType: 'json',
-    //     success: function (data) {
-    //         console.log(data);
-    //     },
-    //     error: function (err) {
-    //         console.err(err);
 
-    //     }
-    // });
+    const loadTweets = () => {
+        $.ajax({
+            url: '/tweets',
+            method: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (err) {
+                console.err(err);
 
-    const tweetData = {
-        "user": {
-            "name": "Newton",
-            "avatars": "https://i.imgur.com/73hZDYK.png",
-            "handle": "@SirIsaac"
-        },
-        "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-        "created_at": 1461116232227
+            }
+        });
     }
 
-    function createTweetElement(post){
+    loadTweets();
+
+
+    const data = [
+        {
+            "user": {
+                "name": "Newton",
+                "avatars": "https://i.imgur.com/73hZDYK.png"
+                ,
+                "handle": "@SirIsaac"
+            },
+            "content": {
+                "text": "If I have seen further it is by standing on the shoulders of giants"
+            },
+            "created_at": 1461116232227
+        },
+        {
+            "user": {
+                "name": "Descartes",
+                "avatars": "https://i.imgur.com/nlhLi3I.png",
+                "handle": "@rd"
+            },
+            "content": {
+                "text": "Je pense , donc je suis"
+            },
+            "created_at": 1461113959088
+        }
+    ]
+
+
+    const createTweetElement = function (obj) {
         const $location = $('#tweets-container');
-        const $header = $(`<header> <div> <p><img src="${tweetData.user.avatars}" alt=""> ${tweetData.user.name}</p><p class="handle">${tweetData.user.handle}</p></div></header>`);
+        const $header = $(`<header> <div> <p><img src="${obj.user.avatars}" alt=""> ${obj.user.name}</p><p class="handle">${obj.user.handle}</p></div></header>`);
 
         const $div = $('<div>');
-        const $footer = $('<footer>')
+        const $footer = $('<footer>');
 
+        const $article = $('<article>');
         const $logos = $(`<div class="logos"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i> </div>`)
-        const $date = $(`<div class="date">${tweetData.created_at}</div>`);
-        const $content = $(`<p>${tweetData.content.text}</p>`);
+        const $date = $(`<div class="date need_to_be_rendered" datetime="${obj.created_at}"></div>`);
+        const $content = $(`<p>${obj.content.text}</p>`);
         const $tweetInfo = $div.append($content);
         const $footerInfo = $footer.append($date, $logos);
-        const $complete = $location.append($header, $tweetInfo, $footerInfo);
 
+        const $articles = $article.append($header, $tweetInfo, $footerInfo);
+
+        const $complete = $location.append($articles);
         return $complete;
-
     };
 
- 
 
-    const $tweet = createTweetElement(tweetData);
+    const renderTweets = function (tweets) {
+        for (const tweet in tweets) {
+            // console.log(tweets[tweet]);
+            createTweetElement(tweets[tweet]);
+        }
 
-    // Test / driver code (temporary)
-    console.log($tweet); // to see what it looks like
-    $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    }
 
+    renderTweets(data);
 
+    timeago.render(document.querySelectorAll('.need_to_be_rendered'));
 
-    console.log($tweet);
+    const $form = $('#new-tweet');
+
+    $form.on('submit', function (e) {
+        e.preventDefault();
+
+        const serializeData = $(this).serialize();
+
+        $.post('/tweets', serializeData, (r) => {
+            console.log(r);
+            console.log('yes');
+        })
+        
+    });
+
 })
