e6b7076eac9abe7eafa51a5c1f7f9455e91da0c8
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-10-08 02:20:23 +0000

    adds responsive design and form validator

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -17,6 +17,12 @@ $(document).ready(function () {
 
     loadTweets();
 
+    const escape = function (str) {
+        let div = document.createElement("div");
+        div.appendChild(document.createTextNode(str));
+        return div.innerHTML;
+      };
+
     //creates and prepends data onto index page
     const createTweetElement = function (obj) {
         const formattedDate = timeago.format(obj.created_at);
@@ -24,7 +30,7 @@ $(document).ready(function () {
         const $createLocation = $('#tweets-container');
         const $article = $('<article>');
         const $header = $(`<header> <div> <p><img src="${obj.user.avatars}" alt=""> ${obj.user.name}</p><p class="handle">${obj.user.handle}</p></div></header>`);
-        const $content = $(`<div><p>${obj.content.text}</p></div>`);
+        const $content = $(`<div><p>${escape(obj.content.text)}</p></div>`);
         const $footer = $(`<footer><div class="date need_to_be_rendered" datetime="1633639091411">${formattedDate}</div><div class="logos"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div></footer>`)
 
         const $articles = $article.append($header, $content, $footer);
@@ -44,33 +50,30 @@ $(document).ready(function () {
     }
 
     const $form = $('#new-tweet');
-    $form.on('submit', function (e) {
-        e.preventDefault();
 
-        //jquery validator rules and messages
-        $('#new-tweet').validate({
-            rules: {
-                text: {
-                    required: true,
-                    minlength: 1,
-                    maxlength: 140
-                },
+
+    $form.validate({
+        rules: {
+            text: {
+                required: true,
+                minlength: 1,
+                maxlength: 140
             },
-            messages: {
-                text: {
-                    required: 'Please insert your deep thought above',
-                    minlength: 'Please insert your thought above',
-                    maxlength: 'This hum is too long, please shorten it!'
-                },
-
+        },
+        messages: {
+            text: {
+                required: 'Please insert your deep thought above',
+                minlength: 'Please insert your thought above',
+                maxlength: 'This hum is too long, please shorten it!'
             },
-            submitHandler: function (form) {
-                const serializedData = $(form).serialize();
-                $.post('/tweets', serializedData).then(loadTweets).then($('#tweet-text').val(''), console.log('test'));
-            }
-        });
+        },
 
+        submitHandler: function (form) {
+            console.log('validate');
 
+            const serializedData = $(form).serialize();;
+            $.post('/tweets', serializedData).then(loadTweets).then($('#tweet-text').val(''));
+        }
     });
 
 })
